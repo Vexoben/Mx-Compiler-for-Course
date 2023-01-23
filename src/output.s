d3ba3a41cf88f8fd6e2fde41917dbdb8cc514450
Vexoben <2493583737@qq.com>
2023-01-23 20:59:42 +0800

    update

--- src/output.s
+++ src/output.s
@@ -1,11 +1,162 @@
 	.text
 	.file	"test.mx"
+.globl   qpow
+   .p2align   2
+   .type   qpow,@function
+qpow:
+.qpow.qpowentry:
+   addi sp, sp, -168
+   mv s1, ra
+   sw s1, 0(sp)
+   mv s1, s0
+   sw s1, 4(sp)
+   mv s1, a0
+   sw s1, 8(sp)
+   mv s1, a1
+   sw s1, 12(sp)
+   mv s1, a2
+   sw s1, 16(sp)
+   j .qpow.first_block__qpow
+.qpow.first_block__qpow:
+   lw s0, 8(sp)
+   sw s0, 20(sp)
+   lw s0, 12(sp)
+   sw s0, 24(sp)
+   lw s0, 16(sp)
+   sw s0, 28(sp)
+   li s0, 1
+   sw s0, 32(sp)
+   lw s0, 32(sp)
+   sw s0, 36(sp)
+   lw s1, 20(sp)
+   sw s1, 40(sp)
+   lw s0, 40(sp)
+   sw s0, 44(sp)
+   j .qpow.while_condition
+.qpow.while_condition:
+   lw s1, 24(sp)
+   sw s1, 48(sp)
+   li s0, 0
+   sw s0, 52(sp)
+   lw s0, 52(sp)
+   lw s1, 48(sp)
+   slt s0, s0, s1
+   sw s0, 56(sp)
+   li s0, 0
+   sw s0, 60(sp)
+   lw s0, 48(sp)
+   lw s1, 60(sp)
+   bgt s0, s1, .qpow.while_repeat
+   j .qpow.while_exit
+.qpow.while_repeat:
+   lw s1, 24(sp)
+   sw s1, 64(sp)
+   li s0, 1
+   sw s0, 68(sp)
+   lw s0, 64(sp)
+   lw s1, 68(sp)
+   and s0, s0, s1
+   sw s0, 72(sp)
+   li s0, 1
+   sw s0, 76(sp)
+   lw s0, 72(sp)
+   lw s1, 76(sp)
+   xor s0, s0, s1
+   sw s0, 80(sp)
+   lw s0, 80(sp)
+   sltiu s0, s0, 1
+   sw s0, 84(sp)
+   li s0, 1
+   sw s0, 88(sp)
+   lw s0, 72(sp)
+   lw s1, 88(sp)
+   beq s0, s1, .qpow.if_true
+   j .qpow.if_false
+.qpow.while_exit:
+   lw s1, 36(sp)
+   sw s1, 92(sp)
+   lw s0, 92(sp)
+   mv a0, s0
+   lw s0, 0(sp)
+   mv ra, s0
+   lw s0, 4(sp)
+   mv s0, s0
+   addi sp, sp, 168
+   ret
+   j .qpow.qpowexit
+.qpow.if_true:
+   lw s1, 36(sp)
+   sw s1, 96(sp)
+   lw s1, 36(sp)
+   sw s1, 100(sp)
+   lw s1, 44(sp)
+   sw s1, 104(sp)
+   lw s0, 100(sp)
+   lw s1, 104(sp)
+   mul s0, s0, s1
+   sw s0, 108(sp)
+   lw s1, 28(sp)
+   sw s1, 112(sp)
+   lw s0, 108(sp)
+   lw s1, 112(sp)
+   rem s0, s0, s1
+   sw s0, 116(sp)
+   lw s0, 116(sp)
+   sw s0, 36(sp)
+   j .qpow.if_exit
+.qpow.if_false:
+   j .qpow.if_exit
+.qpow.if_exit:
+   lw s1, 44(sp)
+   sw s1, 120(sp)
+   lw s1, 44(sp)
+   sw s1, 124(sp)
+   lw s1, 44(sp)
+   sw s1, 128(sp)
+   lw s0, 124(sp)
+   lw s1, 128(sp)
+   mul s0, s0, s1
+   sw s0, 132(sp)
+   lw s1, 28(sp)
+   sw s1, 136(sp)
+   lw s0, 132(sp)
+   lw s1, 136(sp)
+   rem s0, s0, s1
+   sw s0, 140(sp)
+   lw s0, 140(sp)
+   sw s0, 44(sp)
+   lw s1, 24(sp)
+   sw s1, 144(sp)
+   lw s1, 24(sp)
+   sw s1, 148(sp)
+   li s0, 2
+   sw s0, 152(sp)
+   lw s0, 148(sp)
+   lw s1, 152(sp)
+   div s0, s0, s1
+   sw s0, 156(sp)
+   lw s0, 156(sp)
+   sw s0, 24(sp)
+   j .qpow.while_condition
+.qpow.qpowexit:
+   lw s1, 160(sp)
+   sw s1, 164(sp)
+   lw s0, 164(sp)
+   mv a0, s0
+   lw s0, 0(sp)
+   mv ra, s0
+   lw s0, 4(sp)
+   mv s0, s0
+   addi sp, sp, 168
+   ret
+.Lfunc_end0:
+   .size   qpow, .Lfunc_end0-qpow
 .globl   main
    .p2align   2
    .type   main,@function
 main:
 .main.mainentry:
-   addi sp, sp, -60
+   addi sp, sp, -52
    mv s1, ra
    sw s1, 0(sp)
    mv s1, s0
@@ -13,70 +164,67 @@ main:
    call __init_function__
    j .main.first_block__main
 .main.first_block__main:
-   li s0, 0
+   li s0, 2
    sw s0, 8(sp)
-   la s0, .const_str
+   lw s0, 8(sp)
+   mv a0, s0
+   li s0, 10
    sw s0, 12(sp)
    lw s0, 12(sp)
-   lw s1, 8(sp)
-   add s0, s0, s1
-   sw s0, 16(sp)
-   li s0, 0
-   sw s0, 20(sp)
-   la s0, .const_str2
-   sw s0, 24(sp)
-   lw s0, 24(sp)
-   lw s1, 20(sp)
-   add s0, s0, s1
-   sw s0, 28(sp)
-   lw s0, 16(sp)
-   mv a0, s0
-   lw s0, 28(sp)
    mv a1, s0
-   call __build_in_str_add
+   li s0, 10000
+   sw s0, 16(sp)
+   lw s0, 16(sp)
+   mv a2, s0
+   call qpow
    mv s1, a0
+   sw s1, 20(sp)
+   lw s0, 20(sp)
+   mv a0, s0
+   call toString
+   mv s1, a0
+   sw s1, 24(sp)
+   lw s0, 24(sp)
+   sw s0, 28(sp)
+   lw s1, 28(sp)
    sw s1, 32(sp)
    lw s0, 32(sp)
-   sw s0, 36(sp)
-   lw s1, 36(sp)
-   sw s1, 40(sp)
-   lw s0, 40(sp)
    mv a0, s0
    call println
    li s0, 0
-   sw s0, 44(sp)
-   lw s0, 44(sp)
+   sw s0, 36(sp)
+   lw s0, 36(sp)
    mv a0, s0
    lw s0, 0(sp)
    mv ra, s0
    lw s0, 4(sp)
    mv s0, s0
-   addi sp, sp, 60
+   addi sp, sp, 52
    ret
    j .main.mainexit
 .main.mainexit:
    li s0, 0
-   sw s0, 48(sp)
+   sw s0, 40(sp)
+   lw s0, 40(sp)
+   mv a0, s0
+   lw s0, 0(sp)
+   mv ra, s0
+   lw s0, 4(sp)
+   mv s0, s0
+   addi sp, sp, 52
+   ret
+   lw s1, 44(sp)
+   sw s1, 48(sp)
    lw s0, 48(sp)
    mv a0, s0
    lw s0, 0(sp)
    mv ra, s0
    lw s0, 4(sp)
    mv s0, s0
-   addi sp, sp, 60
+   addi sp, sp, 52
    ret
-   lw s1, 52(sp)
-   sw s1, 56(sp)
-   lw s0, 56(sp)
-   mv a0, s0
-   lw s0, 0(sp)
-   mv ra, s0
-   lw s0, 4(sp)
-   mv s0, s0
-   addi sp, sp, 60
-   ret
-.Lfunc_end0:
-   .size   main, .Lfunc_end0-main
+.Lfunc_end1:
+   .size   main, .Lfunc_end1-main
 .globl   __init_function__
    .p2align   2
    .type   __init_function__,@function
@@ -95,14 +243,7 @@ __init_function__:
    mv s0, s0
    addi sp, sp, 8
    ret
-.Lfunc_end1:
-   .size   __init_function__, .Lfunc_end1-__init_function__
-   .type   .const_str2,@object
-.section   .rodata.str1.1,"aMS",@progbits,1
-.const_str2:
-   .asciz   "B"
-   .type   .const_str,@object
-.const_str:
-   .asciz   "a"
+.Lfunc_end2:
+   .size   __init_function__, .Lfunc_end2-__init_function__
 	.ident	"clang version 10.0.0-4ubuntu1 "
 	.section	".note.GNU-stack","",@progbits
--- src/test_without_allocate.s
+++ src/test_without_allocate.s
@@ -1,68 +1,139 @@
 	.text
 	.file	"test.mx"
+.globl   qpow
+   .p2align   2
+   .type   qpow,@function
+qpow:
+.qpow.qpowentry:
+   mv virtual_ra, ra
+   mv virtual_s0, s0
+   mv qpow_a, a0
+   mv qpow_p, a1
+   mv qpow_mod, a2
+   j .qpow.first_block__qpow
+.qpow.first_block__qpow:
+   sw qpow_a, 0(a)
+   sw qpow_p, 0(p)
+   sw qpow_mod, 0(mod)
+   li tmp, 1
+   sw tmp, 0(t)
+   lw load_inst, 0(a)
+   sw load_inst, 0(y)
+   j .qpow.while_condition
+.qpow.while_condition:
+   lw load_inst2, 0(p)
+   li tmp2, 0
+   slt , tmp2, load_inst2
+   li tmp3, 0
+   bgt load_inst2, tmp3, .qpow.while_repeat
+   j .qpow.while_exit
+.qpow.while_repeat:
+   lw load_inst3, 0(p)
+   li tmp4, 1
+   and 2, load_inst3, tmp4
+   li tmp5, 1
+   xor tmp6, 2, tmp5
+   sltiu 3, tmp6, 1
+   li tmp7, 1
+   beq 2, tmp7, .qpow.if_true
+   j .qpow.if_false
+.qpow.while_exit:
+   lw load_inst4, 0(t)
+   mv a0, load_inst4
+   mv ra, virtual_ra
+   mv s0, virtual_s0
+   ret
+   j .qpow.qpowexit
+.qpow.if_true:
+   lw load_inst5, 0(t)
+   lw load_inst6, 0(t)
+   lw load_inst7, 0(y)
+   mul 4, load_inst6, load_inst7
+   lw load_inst8, 0(mod)
+   rem 5, 4, load_inst8
+   sw 5, 0(t)
+   j .qpow.if_exit
+.qpow.if_false:
+   j .qpow.if_exit
+.qpow.if_exit:
+   lw load_inst9, 0(y)
+   lw load_inst10, 0(y)
+   lw load_inst11, 0(y)
+   mul 6, load_inst10, load_inst11
+   lw load_inst12, 0(mod)
+   rem 7, 6, load_inst12
+   sw 7, 0(y)
+   lw load_inst13, 0(p)
+   lw load_inst14, 0(p)
+   li tmp8, 2
+   div 8, load_inst14, tmp8
+   sw 8, 0(p)
+   j .qpow.while_condition
+.qpow.qpowexit:
+   lw load_inst15, 0(allo)
+   mv a0, load_inst15
+   mv ra, virtual_ra
+   mv s0, virtual_s0
+   ret
+.Lfunc_end0:
+   .size   qpow, .Lfunc_end0-qpow
 .globl   main
    .p2align   2
    .type   main,@function
 main:
 .main.mainentry:
-   mv virtual_ra, ra
-   mv virtual_s0, s0
+   mv virtual_ra2, ra
+   mv virtual_s02, s0
    call __init_function__
    j .main.first_block__main
 .main.first_block__main:
-   li tmp, 0
-   la str.addr, .const_str
-   add get_element_ptr_inst, str.addr, tmp
-   li tmp2, 0
-   la str.addr2, .const_str2
-   add get_element_ptr_inst2, str.addr2, tmp2
-   mv a0, get_element_ptr_inst
-   mv a1, get_element_ptr_inst2
-   call __build_in_str_add
+   li tmp9, 2
+   mv a0, tmp9
+   li tmp10, 10
+   mv a1, tmp10
+   li tmp11, 10000
+   mv a2, tmp11
+   call qpow
    mv function_call_inst, a0
-   sw function_call_inst, 0(allo_inst)
-   lw load_inst, 0(allo_inst)
-   mv a0, load_inst
+   mv a0, function_call_inst
+   call toString
+   mv function_call_inst2, a0
+   sw function_call_inst2, 0(allo_inst)
+   lw load_inst16, 0(allo_inst)
+   mv a0, load_inst16
    call println
-   li tmp3, 0
-   mv a0, tmp3
-   mv ra, virtual_ra
-   mv s0, virtual_s0
+   li tmp12, 0
+   mv a0, tmp12
+   mv ra, virtual_ra2
+   mv s0, virtual_s02
    ret
    j .main.mainexit
 .main.mainexit:
-   li tmp4, 0
-   mv a0, tmp4
-   mv ra, virtual_ra
-   mv s0, virtual_s0
+   li tmp13, 0
+   mv a0, tmp13
+   mv ra, virtual_ra2
+   mv s0, virtual_s02
    ret
-   lw load_inst2, 0(allo)
-   mv a0, load_inst2
-   mv ra, virtual_ra
-   mv s0, virtual_s0
+   lw load_inst17, 0(allo2)
+   mv a0, load_inst17
+   mv ra, virtual_ra2
+   mv s0, virtual_s02
    ret
-.Lfunc_end0:
-   .size   main, .Lfunc_end0-main
+.Lfunc_end1:
+   .size   main, .Lfunc_end1-main
 .globl   __init_function__
    .p2align   2
    .type   __init_function__,@function
 __init_function__:
 .__init_function__.__init_function__entry:
-   mv virtual_ra2, ra
-   mv virtual_s02, s0
+   mv virtual_ra3, ra
+   mv virtual_s03, s0
    j .__init_function__.__init_function__exit
 .__init_function__.__init_function__exit:
-   mv ra, virtual_ra2
-   mv s0, virtual_s02
+   mv ra, virtual_ra3
+   mv s0, virtual_s03
    ret
-.Lfunc_end1:
-   .size   __init_function__, .Lfunc_end1-__init_function__
-   .type   .const_str2,@object
-.section   .rodata.str1.1,"aMS",@progbits,1
-.const_str2:
-   .asciz   "B"
-   .type   .const_str,@object
-.const_str:
-   .asciz   "a"
+.Lfunc_end2:
+   .size   __init_function__, .Lfunc_end2-__init_function__
 	.ident	"clang version 10.0.0-4ubuntu1 "
 	.section	".note.GNU-stack","",@progbits
